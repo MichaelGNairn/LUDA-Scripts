8d30cedcab8041bcb4d8eeb18126aa72089631c3
MichaelGNairn <96240844+MichaelGNairn@users.noreply.github.com>
2022-09-15 15:08:49 +0100

    Update and rename Michael Nairn Healthy Life Expectancy (female) R Script.R to Healthy Life Expectancy (female) R Script.R

--- Webscraping/Healthy Life Expectancy (HLE)/Healthy Life Expectancy (female) R Script.R
+++ Webscraping/Healthy Life Expectancy (HLE)/Healthy Life Expectancy (female) R Script.R
@@ -24,7 +24,7 @@ Metric_short <- "HLEFemale"
 #### Import data from web ####
 
 # Set Working directory to place webscraped data files into
-setwd("D:/Coding_Repos/LUDA/Webscraping/Webscraped Inputs") # please note this path will be specific for your local drive
+setwd("D:/Coding_Repos/LUDA-Scripts/Webscraping/Webscraped Inputs") # please note this path will be specific for your local drive
 
 
 scraped_data <- "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/healthandlifeexpectancies/datasets/healthstatelifeexpectancyallagesuk"
@@ -153,7 +153,7 @@ csv_output <-  scraped_clean %>%
 
 # SET WORKING DIRECTORY!! 
 
-setwd("D:/Coding_Repos/LUDA") # please note this path will be specific for your local drive
+setwd("D:/Coding_Repos/LUDA-Scripts") # please note this path will be specific for your local drive
 
 output_folder <- "Output"
 
